8e25d7644877a8a346c7b0299b55fc3159bbb193
Estyn Edwards <estyn@punchcard.io>
2021-10-09 13:09:48 -0600

    added keys

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 /** Cached keys used to validate SHC for some specific issuers */
 const keys = {
-	"https://covid19.quebec.ca/PreuveVaccinaleApi/issuer" : {
+	"https://covid19.quebec.ca/PreuveVaccinaleApi/issuer": {
 		kid: "Nqa1zvChOkoA46B5ZM_oK3MDhL3-mnLERV_30vkHQIc",
 		alg: "ES256",
 		kty: "EC",
@@ -9,17 +9,22 @@ const keys = {
 		x: "XSxuwW_VI_s6lAw6LAlL8N7REGzQd_zXeIVDHP_j_Do",
 		y: "88-aI4WAEl4YmUpew40a9vq_w5OcFvsuaKMxJRLRLL0",
 	},
-	"https://smarthealthcard.phsa.ca/v1/issuer" : {
+	"https://smarthealthcard.phsa.ca/v1/issuer": {
 		kid: "XCqxdhhS7SWlPqihaUXovM_FjU65WeoBFGc_ppent0Q",
 		alg: "ES256",
 		kty: "EC",
 		crv: "P-256",
 		use: "sig",
 		x: "xscSbZemoTx1qFzFo-j9VSnvAXdv9K-3DchzJvNnwrY",
-      	y: "jA5uS5bz8R2nxf_TU-0ZmXq6CKWZhAG1Y4icAx8a9CA"
+		y: "jA5uS5bz8R2nxf_TU-0ZmXq6CKWZhAG1Y4icAx8a9CA"
+	},
+	"https://covidrecords.alberta.ca/smarthealth/issuer": {
+		kty: "EC",
+		kid: "JoO-sJHpheZboXdsUK4NtfulfvpiN1GlTdNnXN3XAnM",
+		use: "sig", alg: "ES256", crv: "P-256", x: "GsriV0gunQpl2X9KgrDZ4EDCtIdfOmdzhdlosWrMqKk", y: "S99mZMCcJRsn662RaAmk_elvGiUs8IvSA7qBh04kaw0"
 	}
 }
 
 module.exports = {
-    keys
+	keys
 }
